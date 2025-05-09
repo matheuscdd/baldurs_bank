5c6574d0d4d884dc21eedde36f99e61b932c5123
matheuscdd <matheuscdd@hotmail.com>
2025-05-09 15:46:25 -0300

    feat: standardized template

---
+++ frontend/app/src/app/layout/component/app.topbar/app.topbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { StyleClassModule } from 'primeng/styleclass';
+import { AppConfiguratorComponent } from '../app.configurator/app.configurator.component';
+import { LayoutService } from '../../service/layout.service';
+
+@Component({
+    selector: 'app-topbar',
+    standalone: true,
+    imports: [RouterModule, CommonModule, StyleClassModule, AppConfiguratorComponent],
+    templateUrl: './app.topbar.component.html',
+    styleUrl: './app.topbar.component.scss'
+})
+export class AppTopbarComponent {
+    items!: MenuItem[];
+
+    constructor(public layoutService: LayoutService) {}
+
+    toggleDarkMode() {
+        this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+    }
+}
