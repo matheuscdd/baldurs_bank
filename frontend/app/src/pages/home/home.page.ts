694e7ecf6bdc4e349b6f93f30ebbfb2b70b29c51
Claus <103610806+matheuscdd@users.noreply.github.com>
2025-05-14 07:51:02 -0300

    feat: active account (#6)

--- frontend/app/src/pages/home/home.page.ts
+++ frontend/app/src/pages/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppTopbarComponent } from '../../shared/app.topbar/app.topbar.component';
+import { ActiveAccountComponent } from '../../features/active.account/components/main/active.account/active.account.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [AppTopbarComponent],
+  imports: [AppTopbarComponent, ActiveAccountComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
