69137737449b77ec788b5bf534a74ccc5d1046a7
Claus <103610806+matheuscdd@users.noreply.github.com>
2025-05-14 23:39:08 -0300

    fix: date filter (#11)

--- frontend/app/src/features/transactions.manager/components/statement/statement.component.ts
+++ frontend/app/src/features/transactions.manager/components/statement/statement.component.ts
@@ -37,7 +37,7 @@ export class StatementComponent {
   }
 
   onSearch() {
-    if (!this.rangeDates || this.rangeDates.length !== 2) return;
+    if (!this.rangeDates || this.rangeDates.filter(Boolean).length !== 2) return;
     this.blockBtn = true;
     this.rangeDates[1].setHours(23, 59, 0, 0); 
     const [startDate, endDate] = this.rangeDates.map(this.formatDate);
